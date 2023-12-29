5eed4cadf8c1a27f1c6380c7e01d2a045b506296
zotic1001 <zotic1001@gmail.com>
2023-12-29 21:53:08 +0300

    Add word API logic, add words_repo.py. Resolve models

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ class AppSetting(BaseSettings):
 
     log_level: str = 'DEBUG'
     db: str = 'postgresql://username:password@db:5432/mydatabase'
-
+    api_key: str = '52dc615079msh293b0674f650d42p1b81fajsncdb66021277d'
     class Config:
         env_prefix = 'APP_'
 
--- db/models.py
+++ db/models.py
@@ -4,21 +4,24 @@ from sqlalchemy.dialects.postgresql import UUID
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = "users"
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, index=True)
+    user_name = Column(String, unique=True, index=True)
     password_hash = Column(String)
 
 
 class Word(Base):
     __tablename__ = "words"
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
+    word_id = Column(UUID(as_uuid=True), primary_key=True, index=True)
     title = Column(String)
-    category = Column(ForeignKey("categories.id"))
+    translation = Column(String)
 
 
-class Category(Base):
-    __tablename__ = "categories"
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
-    name = Column(String)
+association_table2 = Table(
+    "association_table_words_and_users",
+    Base.metadata,
+    Column("words", ForeignKey("words.word_id")),
+    Column("users", ForeignKey("users.user_id")),
+)
