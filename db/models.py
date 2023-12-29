b8353c58a633874a7881a73fc1cb77fdf7919c85
zotic1001 <zotic1001@gmail.com>
2023-12-29 15:21:32 +0300

    Add Word model, fix manager.py

--- db/models.py
+++ db/models.py
@@ -1,17 +1,33 @@
 from datetime import datetime
 
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
 
 Base = declarative_base()
 
 
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    category_name = Column(String, nullable=False)
+
+
+class Word(Base):
+    __tablename__ = "words"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    text = Column(String, nullable=False)
+    category = Column(Integer, ForeignKey("categories.id"))
+
+
 class User(Base):
     __tablename__ = "users"
-    id = Column(UUID, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, nullable=False)
     username = Column(String, nullable=False)
     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey("words.id"))
     hashed_password: str = Column(String(length=1024), nullable=False)
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
--- src/homework/api/auth/schemas.py
+++ src/homework/api/auth/schemas.py
@@ -6,9 +6,12 @@ from fastapi_users import schemas
 
 class UserRead(schemas.BaseUser[int]):
     id: int
+    email: str
     username: str
+    role_id: int
     is_active: bool = True
     is_superuser: bool = False
+    is_verified: bool = False
 
     class Config:
         orm_mode = True
@@ -16,6 +19,9 @@ class UserRead(schemas.BaseUser[int]):
 
 class UserCreate(schemas.BaseUserCreate):
     username: str
+    email: str
     password: str
+    role_id: int
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
