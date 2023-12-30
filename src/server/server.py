70c4bb1b7518b6c38582f8c8f3eee8678b7fd5c9
DzAndrei <dzekalo_andrei@mail.ru>
2023-12-30 13:30:37 +0300

    Add exception in auth and reg

--- src/server/server.py
+++ src/server/server.py
@@ -1,15 +1,12 @@
 import logging.config
 
-from faker import Faker
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config.settings import app_settings
 from .contracts import Token, AuthAttributes
 from db.users import UsersRepo
-
-
-fake = Faker()
+from sqlalchemy.exc import IntegrityError
 
 app = FastAPI()
 
@@ -35,16 +32,34 @@ SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 
 @app.post("/auth", response_model=Token)
-def authorization(auth_attributes: AuthAttributes) -> Token:
-    with SessionLocal() as session:
-        users_table = UsersRepo(session)
-        return users_table.login_user(auth_attributes)
+async def authorization(auth_attributes: AuthAttributes) -> HTTPException | Token:
+    try:
+        with SessionLocal() as session:
+            users_table = UsersRepo(session)
+            token = users_table.login_user(auth_attributes)
+            if token is None:
+                raise HTTPException(status_code=404, detail="User not found")
+            return token
+    except Exception as error:
+        raise HTTPException(
+            status_code=500, detail="Internal Server Error"
+        ) from error
 
 
 @app.post("/register", response_model=Token)
-def register(auth_attributes: AuthAttributes) -> Token:
-    with SessionLocal() as session:
-        users_table = UsersRepo(session)
-        token = users_table.add_user(auth_attributes)
-        session.commit()
-    return token
+async def register(auth_attributes: AuthAttributes) -> HTTPException | Token:
+    try:
+        with SessionLocal() as session:
+            users_table = UsersRepo(session)
+            token = users_table.add_user(auth_attributes)
+            session.commit()
+        return token
+    except IntegrityError as error:
+        print(error.params, type(error.params))
+        raise HTTPException(
+            status_code=409, detail="Account already exists"
+        ) from error
+    except Exception as error:
+        raise HTTPException(
+            status_code=500, detail="Internal Server Error"
+        ) from error
