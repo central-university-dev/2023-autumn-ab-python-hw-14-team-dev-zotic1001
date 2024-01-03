5068e9470a4574e5c2ba8f3b148a792d130a2b7a
AndreiDz <139213756+AndreiDz@users.noreply.github.com>
2024-01-04 02:52:43 +0300

    Add flow and linters
    * Add linters, add flow, fix type hints
    
    * Add linters, add flow, fix type hints
    
    * fix docker tests

--- db/users.py
+++ db/users.py
@@ -1,12 +1,12 @@
 import uuid
+from typing import Optional
+
 import jwt
-import bcrypt
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 
 from config.settings import app_settings
-from typing import Optional
-from sqlalchemy.orm import Session
-from . import models
+from db import models
 from src.server.contracts import AuthAttributes, Token
 
 
@@ -21,7 +21,9 @@ class UsersRepo:
             .filter(models.User.user_name == auth_attributes.user_name)
             .first()
         )
-        if user is not None and self.pwd_context.verify(auth_attributes.user_password.encode(), user.password_hash):
+        if user is not None and self.pwd_context.verify(
+            auth_attributes.user_password.encode(), user.password_hash
+        ):
             encoded_jwt = jwt.encode(
                 {'user_id': str(user.user_id), 'user_name': user.user_name},
                 app_settings.secret_key,
@@ -30,12 +32,13 @@ class UsersRepo:
             return Token(token=encoded_jwt)
         return None
 
-    def add_user(self, auth_attributes: AuthAttributes) -> Optional[Token]:
-        print(auth_attributes.user_password.encode())
+    def add_user(self, auth_attributes: AuthAttributes) -> Token:
         user = models.User(
-            user_id=uuid.uuid4(),
+            user_id=uuid.uuid4(),  # type: ignore
             user_name=auth_attributes.user_name,
-            password_hash=self.pwd_context.hash(auth_attributes.user_password.encode()),
+            password_hash=self.pwd_context.hash(
+                auth_attributes.user_password.encode()
+            ),
         )
         self.db.add(user)
         encoded_jwt = jwt.encode(
